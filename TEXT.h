b3d08d5ea8496eb55e07ecf92feb97c65b766ce5
nikodemin <niko.demin@gmail.com>
2017-04-06 23:18:36 +0300

    Create TEXT.h

---
+++ TEXT.h
@@ -0,0 +1,66 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by TEXT.rc
+//
+#define IDD_ABOUT                       5
+#define IDS_GAME                        101
+#define IDS_SAVE_GAME                   102
+#define IDS_LOAD_GAME                   103
+#define IDS_SETTINGS                    104
+#define IDS_NEW_GAME_AI                 105
+#define IDS_NEW_GAME_H                  106
+#define IDD_COLOR_SETTINGS              107
+#define IDS_SAVE_QUESTION               107
+#define IDS_COLOR_SETTINGS              108
+#define IDD_SAVE_GAME                   109
+#define IDS_ERROR                       109
+#define IDD_LOAD_GAME                   110
+#define IDS_MESSAGE                     110
+#define IDS_ABOUT                       111
+#define IDD_NEW_GAME                    111
+#define IDS_WHITE_COURSE                112
+#define IDD_FIGURE_SELECT               112
+#define IDS_BLACK_COURSE                113
+#define IDS_WELCOME                     114
+#define IDC_STATIC_1                    1005
+#define IDC_STATIC_2                    1006
+#define IDC_DONATE                      1007
+#define IDS_GREEN_1                     1008
+#define IDS_RED_1                       1009
+#define IDS_BLUE_1                      1010
+#define IDC_STATIC_3                    1011
+#define IDC_EDIT                        1012
+#define IDC_STATIC_7                    1012
+#define IDSAVE                          1013
+#define IDC_STATIC_9                    1013
+#define IDS_GREEN_2                     1014
+#define IDC_COMBO                       1014
+#define IDS_RED_2                       1015
+#define IDLOAD                          1015
+#define IDS_BLUE_2                      1016
+#define IDC_LOAD_GAME                   1016
+#define IDC_SAVE_GAME                   1017
+#define IDS_GREEN_3                     1018
+#define IDC_DELETE                      1018
+#define IDS_RED_3                       1019
+#define IDR_QUEEn                       1019
+#define IDS_BLUE_3                      1020
+#define IDR_ROOK                        1020
+#define IDC_STATIC_6                    1021
+#define IDR_BLACk                       1021
+#define IDR_HORSE                       1021
+#define IDC_STATIC_8                    1022
+#define IDC_RADIO4                      1022
+#define IDR_WHITE                       1022
+#define IDR_OFFICER                     1022
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        113
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1023
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
