a3c01fa0a7a35288c804603cb4cc1884da0bfaca
nikodemin <niko.demin@gmail.com>
2017-04-06 23:15:37 +0300

    Create stdafx.cpp

---
+++ stdafx.cpp
@@ -0,0 +1,29 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+#define BLACK 1
+#define WHITE 0
+#define aftersel 1
+#define firstsel 2
+#define aftersel 1
+const int CELL = 80;
+#define ID_STATUS 80
+#pragma once
+// Windows Header Files:
+#include <windows.h>
+#include <fstream>
+#include <CommCtrl.h>
+#include <typeinfo>
+#include "resource.h"
+#include "TEXT.h"
+#pragma comment (lib,"comctl32.lib")
+// C RunTime Header Files
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+
+// TODO: reference additional headers your program requires here
