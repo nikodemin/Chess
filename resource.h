778c81391506d8a80d35aa0e7f3bf83657619ee7
nikodemin <niko.demin@gmail.com>
2017-04-06 23:16:46 +0300

    Create resource.h

---
+++ resource.h
@@ -0,0 +1,28 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+//
+#define ID_B_HORSE                      102
+#define ID_B_KING                       104
+#define IDB_BITMAP1                     105
+#define ID_W_ROOK                       105
+#define ID_B_OFFICER                    106
+#define ID_B_PAWN                       109
+#define ID_B_ROOK                       110
+#define ID_B_QUEEN                      111
+#define ID_W_HORSE                      112
+#define ID_W_KING                       113
+#define ID_W_OFFICER                    114
+#define ID_W_PAWN                       115
+#define ID_W_QUEEN                      116
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        123
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
